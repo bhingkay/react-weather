5ccd91315c6d3ce2dee2d6beb2bf516508e27161
bhingkay <79082266+bhingkay@users.noreply.github.com>
2021-03-31 17:04:40 +0300

    added form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Weather from "./Weather";
+import { Button } from "react-bootstrap";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <div className="WeatherApp">
-        <h1>Weather React</h1>
         <Weather />
+        <h1>Jerusalem</h1>
+        <ul>
+          <li>Sunday 12:00</li>
+          <li>Sunny</li>
+        </ul>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./Weather.css";
+import { Button } from "react-bootstrap";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function Weather() {
   return (
     <div>
       <form claswsName="Weather">
-        <input type="search" placeholder="Input a city. . " autoFocus="off" />
-        <input type="submit" value="Search" className="searchbtn" />
+        <input
+          type="search"
+          placeholder="Enter a city "
+          autoFocus="off"
+          className="inputcity"
+        />
+        <Button variant="outline-primary" className="btnprimary">
+          Search
+        </Button>{" "}
+        <Button variant="outline-success" className="currentbtn">
+          Curent
+        </Button>{" "}
       </form>
     </div>
   );
