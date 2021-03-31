1d8fb1c0fb817907fb807c1c6210a95607629a1d
bhingkay <79082266+bhingkay@users.noreply.github.com>
2021-03-31 19:02:45 +0300

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,11 @@ import React from "react";
 export default function Footer() {
   return (
     <small className="footer">
-      <a href="https://github.com/bhingkay/react-weather" target="_blank">
+      <a
+        href="https://github.com/bhingkay/react-weather"
+        target="_blank"
+        rel="noreferrer"
+      >
         Open source code by{" "}
       </a>{" "}
       Aljeve Rudenas
